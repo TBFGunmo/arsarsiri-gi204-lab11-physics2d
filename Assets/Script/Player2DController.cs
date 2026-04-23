13fb069aaaa4012d4ee7f22c31d27bebf58a7df7
TBFGunmo <gunmohome123@gmail.com>
2026-04-23 10:59:37 +0700

    finish class

---
+++ Assets/Script/Player2DController.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player2DController : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public float jumpForce = 450;
+
+    private Rigidbody2D rb;
+    private float moveInputValue;
+
+    private bool isGrounded;
+
+    private SpriteRenderer SpriteRenderer;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+        SpriteRenderer = rb.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Keyboard.current != null) 
+        {
+            moveInputValue = (Keyboard.current.dKey.isPressed ? 1 : 0) - (Keyboard.current.aKey.isPressed ? 1 : 0) ;
+        }
+
+        rb.linearVelocity = new Vector2(moveInputValue * speed, rb.linearVelocity.y);
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded) 
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jumpForce));
+        }
+
+        if (moveInputValue < 0 ) 
+        {
+           SpriteRenderer.flipX = true;
+        }
+        if (moveInputValue > 0) 
+        {
+            SpriteRenderer.flipX = false;
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) 
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+}
